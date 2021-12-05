3f307292b93613c4bf741146bdb016eecd3ef6bb
ckdals20213063 <84118069+ckdals20213063@users.noreply.github.com>
2021-12-05 22:08:50 +0900

    추가 오류 수정

--- puzzle_make.py
+++ puzzle_make.py
@@ -1,6 +1,7 @@
 from word import Word
 from puzzle_load_words import Puzzle_load_words
 import random
+import copy
 
 class Puzzle_make:
     def __init__(self):
@@ -25,17 +26,24 @@ class Puzzle_make:
 
         location_x = [i for i in range(len(word1))] ## puzzle에 word1의 문자를 표기할때 뽑을 인덱스를 저장할 리스트
         location_y = [i for i in range(len(word2))] ## puzzle에 word2의 문자를 표기할때 뽑을 인덱스를 저장할 리스트
-        location_x.remove(common_x) # 겹치는 문자를 제외하고 뽑으므로 삭제
-        location_y.remove(common_y) # 겹치는 문자를 제외하고 뽑으므로 삭제
-        
-        self.res_puzzle = [['#']*len(word1) for _ in range(len(word2))]
+
         #수정
+
+        self.res_puzzle = [['#']*len(word1) for _ in range(len(word2))]
         i = 0
-        while i < len(word1)-1:
+        while i < len(word1):
                 self.puzzle_board[common_y][location_x[i]] = '_'
                 self.res_puzzle[common_y][location_x[i]] = word1[i]
                 i += 1
+        i = 0
+        while i < len(word2):
+                self.puzzle_board[location_y[i]][common_x] = '_'
+                self.res_puzzle[location_y[i]][common_x] = word2[i]
+                i += 1
         #
+
+        location_x.remove(common_x) # 겹치는 문자를 제외하고 뽑으므로 삭제
+        location_y.remove(common_y) # 겹치는 문자를 제외하고 뽑으므로 삭제
 
         i = 0
         while i < (len(word1)-1)/2: # word1의 (len(word1)-1)/2 번 반복하며 puzzle_board에 문자를 표기
@@ -44,15 +52,6 @@ class Puzzle_make:
             location_x.pop(random_index) # 뽑은 index는 다음번 random추출할때 제외하기 위해서 list에서 제거
             i += 1
         
-        #수정
-        i = 0
-        while i < len(word2)-1:
-                self.puzzle_board[location_y[i]][common_x] = '_'
-                self.res_puzzle[location_y[i]][common_x] = word2[i]
-                i += 1
-
-        self.res_puzzle[common_y][common_x]=word2[common_y]
-        #
         i = 0
         while i < (len(word2)-1)/2: #  word2의 (len(word1)-1)/2 번 반복하며 puzzle_board에 문자를 표기
             random_index = random.randrange(0, len(location_y)) # random한 index 뽑기
