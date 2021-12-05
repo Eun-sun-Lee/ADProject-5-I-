4fb697fcb0460ffdda62c08f9bd574268e17214b
ckdals20213063 <84118069+ckdals20213063@users.noreply.github.com>
2021-12-05 20:55:22 +0900

    정답비교용 py 수정

--- res_check.py
+++ res_check.py
@@ -1,12 +1,16 @@
+import copy
+from puzzle_make import Puzzle_make
 class Res_Check:
-    def __init__(self,words):
-        self.word_res = words
-        self.word_input = ""
+    def __init__(self, word):
+        self.word_res = word
+        print(self.word_res)
     def cmp(self, inputs):
-        self.word_input = inputs
-        bool res=True;
-        for i in range(self.word_input):
-            if self.word_input[i]!=self.word_res[i]:
-                res=False
-                break
+        res = True;
+        self.get_inputs = copy.deepcopy(inputs)
+        print(self.get_inputs)
+        for i in range(len(self.word_res)):
+            for j in range(len(self.word_res[i])):
+                if self.word_res[i]!=self.get_inputs[i]:
+                    res=False
+                    break
         return res
